1df3e57aa64b1b3ec61f820a8a43105dd25da9f6
Ryan C. Thompson <rct@thompsonclan.org>
2018-11-19 15:42:34 -0800

    Add README.md

--- named-timer.el
+++ named-timer.el
@@ -19,11 +19,11 @@
 ;; This library provides two new functions, `named-timer-run' and
 ;; `named-timer-idle-run'. They work like `run-with-timer' and
 ;; `run-with-idle-timer', except that they take an additional symbol
-;; in front of all other arguments to use as name of the timer. The
-;; main benefit of using a named timer is that re-using a name
-;; automatically cancels the previous timer that was using that name.
-;; This simplifies the common pattern of cancelling and recreating the
-;; same timer when something changes.
+;; or keyword in front of all other arguments to use as name of the
+;; timer. The main benefit of using a named timer is that re-using a
+;; name automatically cancels the previous timer that was using that
+;; name. This simplifies the common and somewhat error prone pattern
+;; of cancelling and recreating the same timer when something changes.
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
