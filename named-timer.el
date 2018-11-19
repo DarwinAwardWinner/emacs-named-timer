13aa461019ab3ae0f60fbe76f39bb2b0befc1a88
Ryan C. Thompson <rct@thompsonclan.org>
2018-11-19 15:38:34 -0800

    Fix some compiler warnings

--- named-timer.el
+++ named-timer.el
@@ -27,18 +27,18 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or (at
-;; your option) any later version.
+;; This program is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
 ;;
 ;; This program is distributed in the hope that it will be useful, but
 ;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 ;; General Public License for more details.
 ;;
 ;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;; along with GNU Emacs. If not, see <http://www.gnu.org/licenses/>.
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -51,11 +51,48 @@
 (defvar named-timer-table (make-hash-table)
   "A hash table mapping symbols to timers.")
 
+(defsubst named-timer-names ()
+  "Return a list of all timer names in `named-timer-table'."
+  (hash-table-keys named-timer-table))
+(defsubst named-timer-timers ()
+  "Return a list of all named timers in `named-timer-table'.
+
+Note that this may include both regular and idle timers."
+  (hash-table-values named-timer-table))
+
+(defsubst named-timer-get (name)
+  "Return the timer named NAME, if any.
+
+If no timer with the specified name exists, returns nil. See
+`named-timer-table'."
+  (cl-assert (symbolp name))
+  (gethash name named-timer-table))
+
+(defun named-timer-cancel (name &optional _noremove)
+  "Cancel the timer with name NAME if it exists.
+
+If NAME is the name of an active named timer, it is cancelled and
+removed from `named-timer-table'. If no timer with that name
+exists, nothing happens.
+
+The second argument, _NOREMOVE, is intended for internal use
+only. If non-nil, the entry for the cancelled timer in
+`named-timer-table' is not removed. This is used for efficiency
+in cases where that hash table entry is about to be replaced
+anyway, such that deleting it is pointless."
+  (let ((timer (named-timer-get name)))
+    (when timer
+      (timer--check timer)
+      (cancel-timer timer)
+      (unless _noremove
+        (remhash name named-timer-table)))))
+
 (defun named-timer-run (name &rest args)
   "Set up a named timer.
 
 This creates a new timer by calling `run-with-timer' on ARGS,
 then adds that timer to `named-timer-table' with NAME as the key.
+Like `run-with-timer', this returns the created timer.
 
 Only one timer with a given name can be active. Calling this
 function on a name that is already associated with an existing
@@ -79,25 +116,6 @@ This works like `named-timer-run', but passes ARGS to
     (timer--check timer)
     (puthash name timer named-timer-table)))
 
-(defun named-timer-cancel (name &optional _noremove)
-  "Cancel the timer with name NAME if it exists.
-
-If NAME is the name of an active named timer, it is cancelled and
-removed from `named-timer-table'. If no timer with that name
-exists, nothing happens.
-
-The second argument, _NOREMOVE, is intended for internal use
-only. If non-nil, the cancelled timer is not removed from
-`named-timer-table'. This is used for efficiency in cases where
-that hash table entry is about to be replaced anyway, so deleting
-it is pointless."
-  (let ((timer (named-timer-get name)))
-    (when timer
-      (timer--check timer)
-      (cancel-timer timer)
-      (unless _noremove
-        (remhash name named-timer-table)))))
-
 ;; Non-prefixed aliases that match the corresponding function names in
 ;; `timer.el'. Not sure if they should be kept or removed.
 (defalias 'run-with-named-timer 'named-timer-run)
@@ -105,25 +123,16 @@ it is pointless."
 (defalias 'cancel-named-timer 'named-timer-cancel)
 
 (define-advice cancel-timer (:around (orig-fun &rest args) named-timer-cancel)
-  "Allow cancelling named timers by name."
+  "Allow cancelling named timers by name.
+
+This advice allows `cancel-timer' to accept a symbol argument
+instead of a timer object. A symbol argument will be passed
+instead to `named-timer-cancel' to cancel a timer with that
+name."
   (let ((timer-or-name (car args)))
     (if (symbolp timer-or-name)
-        (named-timer-cancel name)
+        (named-timer-cancel timer-or-name)
       (apply orig-fun args))))
-
-(defsubst named-timer-get (name)
-  "Return the timer named NAME, if any.
-
-See `named-timer-table'."
-  (cl-assert (symbolp name))
-  (gethash name named-timer-table))
-
-(defsubst named-timer-names ()
-  "Return a list of all timer names in `named-timer-table'."
-  (hash-table-keys named-timer-table))
-(defsubst named-timer-timers ()
-  "Return a list of all named timers in `named-timer-table'."
-  (hash-table-values named-timer-table))
 
 (provide 'named-timer)
 
